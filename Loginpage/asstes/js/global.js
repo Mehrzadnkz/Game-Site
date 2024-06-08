c7cee0c2b8e3d752b6a3aeb803ee68840c450b31
Mehrzadnkz <156532691+Mehrzadnkz@users.noreply.github.com>
2024-06-08 22:47:54 +0330

    empty

--- Loginpage/asstes/js/global.js
+++ Loginpage/asstes/js/global.js
@@ -1,10 +1,22 @@
 function checkPasswordMatch() {
-    var Password1 = document.getElementById("Password1").value;
+    var password1 = document.getElementById("password1").value;
     var confpass = document.getElementById("confpass").value;
 
-    if (Password1 !== confpass) {
+    if (password1 !== confpass) {
         document.getElementById("eror").innerHTML = "Passwords Doe's Not Match";
     } else {
         document.getElementById("eror").innerHTML = "";
+    }
+}
+function changeButton() {
+    var sw = document.getElementById('sw');
+    var lw = document.getElementById('lw');
+
+    if (sw.style.display === 'block') {
+        sw.style.display = 'none';
+        lw.style.display = 'block';
+    } else {
+        sw.style.display = 'block';
+        lw.style.display = 'none';
     }
 }
