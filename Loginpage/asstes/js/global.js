a7349b2f0a4ee73a8075b8f6f596be11c4db947e
Mehrzadnkz <156532691+Mehrzadnkz@users.noreply.github.com>
2024-06-08 23:12:15 +0330

    empty

--- Loginpage/asstes/js/global.js
+++ Loginpage/asstes/js/global.js
@@ -8,6 +8,8 @@ function checkPasswordMatch() {
         document.getElementById("eror").innerHTML = "";
     }
 }
+
+// Login - Sign Up
 function changeButton() {
     var sw = document.getElementById('sw');
     var lw = document.getElementById('lw');
@@ -18,5 +20,19 @@ function changeButton() {
     } else {
         sw.style.display = 'block';
         lw.style.display = 'none';
+    }
+}
+
+// Forget pass - Login
+function test() {
+    var lw = document.getElementById('lw');
+    var FP = document.getElementById('FP');
+
+    if (lw.style.display === 'block') {
+        lw.style.display = 'none';
+        FP.style.display = 'block';
+    } else {
+        lw.style.display = 'block';
+        FP.style.display = 'none';
     }
 }
